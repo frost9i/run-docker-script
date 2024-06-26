a406415860f75263a9e044115d70cd7fb275abdb
frost9i <frost9i>
2024-06-26 16:17:22 +0300

    fix: zap mount volume

--- containers/security/tools/zap.sh
+++ containers/security/tools/zap.sh
@@ -6,6 +6,7 @@ zap () {
     docker run -it --rm \
     --name ${ZAP_CONTAINER_NAME} \
     --network ${DOCKER_NETWORK_NAME} \
+    --volume ${DOCKER_MY_HOME}/git:/git \
     softwaresecurityproject/zap-stable bash
     # ghcr.io/zaproxy/zaproxy bash
 }
