8efbc06bac94e0134f7d35306967454c064725f5
frost9i <frost9i>
2024-10-24 10:46:16 +0300

    fix: some improvements

--- containers/dev/runtimes.sh
+++ containers/dev/runtimes.sh
@@ -10,7 +10,7 @@ DEV_LIST=(${PYTHON_CONTAINER_NAME}, ${NODEJS_CONTAINER_NAME}, ${MAVEN_CONTAINER_
 nodejs () {
     docker run -it --rm \
     -p 5000:5000 \
-    --name "${NODEJS_CONTAINER_NAME}" \
+    --name ${NODEJS_CONTAINER_NAME}-${1} \
     --network ${DOCKER_NETWORK_NAME} \
     -v "${DOCKER_MY_HOME}/git:/git" \
     node:${1}-alpine \
@@ -20,7 +20,7 @@ nodejs () {
 python () {
     docker run -it \
     --rm \
-    --name ${PYTHON_CONTAINER_NAME} \
+    --name ${PYTHON_CONTAINER_NAME}-${1} \
     --network ${DOCKER_NETWORK_NAME} \
     -v "${DOCKER_MY_HOME}/git:/git" \
     python:${1} \
@@ -40,7 +40,7 @@ maven () {
 openjdk () {
     docker run -it \
     --rm \
-    --name ${OPENJDK_CONTAINER_NAME} \
+    --name ${OPENJDK_CONTAINER_NAME}-${1} \
     --network ${DOCKER_NETWORK_NAME} \
     -v "${DOCKER_MY_HOME}/git:/git" \
     openjdk:${1}-jdk-slim \
--- containers/security/tools/zap.sh
+++ containers/security/tools/zap.sh
@@ -7,6 +7,8 @@ zap () {
     --name ${ZAP_CONTAINER_NAME} \
     --network ${DOCKER_NETWORK_NAME} \
     --volume ${DOCKER_MY_HOME}/git:/git \
+    --publish 8080:8080 \
     softwaresecurityproject/zap-stable bash
     # ghcr.io/zaproxy/zaproxy bash
+    # --user root \
 }
